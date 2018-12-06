36263a45746653ebe830ffb8a5a7c5fc8dfe1baa
Madhusudan14 <45619286+Madhusudan14@users.noreply.github.com>
2018-12-06 15:28:14 +0530

    Update HelloTest.java

--- src/test/java/org/ravi/helloworld/HelloTest.java
+++ src/test/java/org/ravi/helloworld/HelloTest.java
@@ -24,14 +24,14 @@ public class HelloTest {
 		//driver = new FirefoxDriver();
 		System.setProperty("webdriver.chrome.driver","resources\\chromedriver.exe");
 		driver = new ChromeDriver();
-		driver.get("http://localhost:8080/Helloworld.war/");
+		driver.get("http://localhost:8080/HelloWorld/");
 		driver.manage().timeouts().implicitlyWait(1, TimeUnit.MINUTES); 
 	}
 
 	@Test
 	public void f() {
 System.out.println("My firsst Project");
-		text = driver.findElement(By.xpath("html/body/h2"));
+		//text = driver.findElement(By.xpath("html/body/h2"));
 		Assert.assertEquals(driver.getTitle(), "");
 		System.out.println("Test Passed");
 
